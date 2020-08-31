bcfe96a1a0e6149b835ff9094d55cbe4299fc211
A Jordan Simonds <simonds.jordan@gmail.com>
2020-08-30 22:45:53 -0400

    test: Add tests for actions

---
+++ src/__tests__/action.ts
@@ -0,0 +1,50 @@
+import { action } from '../action';
+import { createStore } from '../createStore';
+import { ActionFunction } from '../util/types';
+import { concernPath } from '../globals';
+import { concern } from '../concern';
+
+describe('action', () => {
+  let store: any;
+
+  beforeEach(() => {
+    store = createStore({
+      state: {},
+      init() {
+        const demo1 = action(() => {});
+        const demo2 = action('explicit', () => {});
+
+        const nested = concern('nested', {
+          state: {},
+          init() {
+            return {
+              demo3: action(() => {}),
+            };
+          },
+        });
+
+        return {
+          demo1,
+          demo2,
+          nested,
+        };
+      },
+    });
+  });
+
+  describe('__action_key__', () => {
+    test('default is empty string', () => {
+      const _action: ActionFunction = action(() => {});
+      expect(_action.__action_key__).toBe('');
+    });
+    test('implicit key', () => {
+      expect(store.demo1.__action_key__).toBe('demo1');
+    });
+    test('explicit key', () => {
+      expect(store.demo2.__action_key__).toBe('explicit');
+    });
+    test('nested key', () => {
+      expect(store.nested.demo3.__action_key__).toBe('demo3');
+    });
+  });
+});
