34ffc79b52a00cdeda043c203958fd35e6ae5a92
A Jordan Simonds <simonds.jordan@gmail.com>
2020-11-17 22:00:04 -0500

    feat: Convert to a plugin and top-level injection

--- src/createStore.ts
+++ src/createStore.ts
@@ -1,4 +1,3 @@
-import devtools from '@vue/devtools-api';
 import { reactive, readonly } from 'vue';
 
 import { StoreOptions, Store } from './types';
@@ -7,12 +6,6 @@ import { subscriber, actionSubscriber } from './subscriptions';
 import { guard } from './state';
 import { nameMutations } from './mutation';
 import { nameActions } from './action';
-
-import logger from './util/logger';
-
-declare var __VUE_DEVTOOLS_GLOBAL_HOOK__: any;
-
-const devtoolsLayerId = 'vune-store';
 
 export function createStore<RootState extends object, R>(config: StoreOptions<RootState, R>): Store<RootState, R> {
   setIsInitializing(true);
@@ -29,31 +22,11 @@ export function createStore<RootState extends object, R>(config: StoreOptions<Ro
     state: readonly(state),
     $subscribe: subscriber(state),
     $subscribeAction: actionSubscriber(state),
+    $provideSymbol: Symbol(),
   });
 
   stateStack.pop();
   setIsInitializing(false);
-
-  if (process.env.NODE_ENV === 'development') {
-    __VUE_DEVTOOLS_GLOBAL_HOOK__.once('init', (app: any) => {
-      devtools.setupDevtoolsPlugin(
-        {
-          id: 'store',
-          label: 'Vune',
-          app: __VUE_DEVTOOLS_GLOBAL_HOOK__.apps[0].app,
-        },
-        function (devtools) {
-          console.log('plugin registered');
-          devtools.addTimelineLayer({
-            id: devtoolsLayerId,
-            label: 'Vune',
-            color: 0x2299ff,
-          });
-          logger.devtools = devtools;
-        }
-      );
-    });
-  }
 
   return augmentedStore as any;
 }
--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,48 @@
+import { App } from 'vue';
+import { setupDevtoolsPlugin } from '@vue/devtools-api';
+
 export { computed as getter } from 'vue';
 export { createStore } from './createStore';
 export { module, defineModule } from './module';
 export { mutation } from './mutation';
 export { action } from './action';
 export { storeProvider } from './storeProvider';
+export * from './types';
+
+import logger from './util/logger';
+
+declare var __VUE_DEVTOOLS_GLOBAL_HOOK__: any;
+
+const devtoolsLayerId = 'vune-store';
+
+export default {
+  install(app: App, options: any) {
+    if (!options.store) {
+      throw new Error('No store provided to Vune');
+    }
+
+    if (process.env.NODE_ENV === 'development') {
+      /*@__PURE__*/ __VUE_DEVTOOLS_GLOBAL_HOOK__.once('init', () => {
+        setupDevtoolsPlugin(
+          {
+            id: 'store',
+            label: 'Vune',
+            app,
+          },
+          function (devtools: any) {
+            console.log('plugin registered');
+            devtools.addTimelineLayer({
+              id: devtoolsLayerId,
+              label: 'Vune',
+              color: 0x2299ff,
+            });
+            logger.devtools = devtools;
+          }
+        );
+      });
+    }
+
+    app.provide(options.store.$provideSymbol, options.store);
+    app.config.globalProperties.$store = options.store;
+  },
+};
