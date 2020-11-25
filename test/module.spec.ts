0739995b44cf4ff31e90ff77d47eb1334d65ff94
A Jordan Simonds <simonds.jordan@gmail.com>
2020-11-25 10:03:36 -0500

    test: async module errors

--- test/module.spec.ts
+++ test/module.spec.ts
@@ -49,8 +49,22 @@ describe('module', () => {
             },
           });
 
+          const mod2 = module('mod2', {
+            state: {
+              foo: 2,
+            },
+            async init(state) {
+              await Promise.resolve();
+              return {
+                state,
+                async: true,
+              };
+            },
+          });
+
           return {
             mod1,
+            mod2,
           };
         },
       });
@@ -59,6 +73,55 @@ describe('module', () => {
     it('should add state to top level sate', () => {
       expect(store.$state.foo).toBe(1);
       expect(store.$state.mod1.foo).toBe(2);
+    });
+
+    it('should create an async module', async () => {
+      expect(store.$state.mod2.foo).toBe(2);
+      const mod2 = await store.mod2;
+      expect(mod2.state.foo).toBe(2);
+      expect(mod2.async).toBe(true);
+    });
+  });
+
+  describe('nested async modules', () => {
+    it('should not throw an error if module registered before await', async () => {
+      const store = createStore({
+        state: {},
+        async init() {
+          expect(() => {
+            module('mod3', {
+              state: {},
+              init() {
+                return {};
+              },
+            });
+          }).not.toThrowError();
+          await Promise.resolve();
+          return {};
+        },
+      });
+
+      await store;
+    });
+
+    it('should throw an error if module registered after await', async () => {
+      const store = createStore({
+        state: {},
+        async init() {
+          await Promise.resolve();
+          expect(() => {
+            module('mod3', {
+              state: {},
+              init() {
+                return {};
+              },
+            });
+          }).toThrowError();
+          return {};
+        },
+      });
+
+      await store;
     });
   });
 });
