56afacd45fb67cf512c0414249cce735fc7298bf
A Jordan Simonds <simonds.jordan@gmail.com>
2021-01-14 22:53:42 -0500

    refactor: explicit boolean returns in helpers

--- src/util/helpers.ts
+++ src/util/helpers.ts
@@ -5,23 +5,23 @@ export function isArray(obj: any): obj is Array<any> {
 }
 
 export function isObject(val: unknown): val is Record<any, any> {
-  return val !== null && typeof val === 'object';
+  return Boolean(val && typeof val === 'object');
 }
 
 export function isModule(val: unknown): val is StoreModule<unknown> {
-  return val && typeof val === 'object' && val.hasOwnProperty('__module_key__');
+  return Boolean(val && typeof val === 'object' && val.hasOwnProperty('__module_key__'));
 }
 
 export function isMutation(val: any): val is MutationFunction {
-  return val && typeof val === 'function' && val.hasOwnProperty('__mutation_key__');
+  return Boolean(val && typeof val === 'function' && val.hasOwnProperty('__mutation_key__'));
 }
 
 export function isAction(val: any): val is ActionFunction {
-  return val && typeof val === 'function' && val.hasOwnProperty('__action_key__');
+  return Boolean(val && typeof val === 'function' && val.hasOwnProperty('__action_key__'));
 }
 
 export function isPromise(val: any): val is Promise<any> {
-  return val instanceof Promise;
+  return Boolean(val && val instanceof Promise);
 }
 
 export function convertToHex(color: string): number {
