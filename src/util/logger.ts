0a27988c5e32eedcb7b5e5c9e0eb3a7ba59fefcf
A Jordan Simonds <simonds.jordan@gmail.com>
2021-01-14 22:42:41 -0500

    feat: Add timeline layer info

--- src/util/logger.ts
+++ src/util/logger.ts
@@ -1,4 +1,5 @@
 import { DevtoolsPluginApi } from '@vue/devtools-api';
+import { getCurrentActions } from '../globals';
 interface Logger {
   devtools: null | DevtoolsPluginApi;
   log: (...args: any[]) => void;
@@ -8,10 +9,15 @@ const logger: Logger = {
   devtools: null,
   log(data: any) {
     if (process.env.NODE_ENV === 'development' && this.devtools) {
+      const actions = Array.from(getCurrentActions().values()).map((a) => a.path);
+      const groupId = actions?.[0];
       this.devtools.addTimelineEvent({
         layerId: 'vune-store',
         event: {
           time: Date.now(),
+          title: data.type,
+          subtitle: data.path,
+          groupId,
           data,
           meta: null,
         },
